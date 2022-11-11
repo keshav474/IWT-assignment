3eea0252328f93c26160394e03e3fbc25bdc3559
keshav474 <guptakeshav0828@gmail.com>
2022-11-11 15:05:58 +0530

    assignment 1

---
+++ as1.js
@@ -0,0 +1,17 @@
+console.log("jai mata di");
+const table = document.getElementById("edutable");
+//console log contents of table
+console.log(table);
+document.getElementById("submit-btn").addEventListener("click", fun);  
+function fun() {
+var qualification = document.getElementById("qualification").value;
+var board = document.getElementById("board").value; 
+var percentage = document.getElementById("percentage").value;
+var year = document.getElementById("year").value;
+console.log(qualification, board, percentage, year.length);
+if(qualification != "" && board != "" && percentage != "" && year.length == 4 && percentage <= 100 && percentage >= 0) 
+document.getElementById("edutable").innerHTML +=`<tr> <td>${qualification}</td> <td>${board}</td> <td>${percentage}</td> <td>${year}</td> </tr>`;
+else
+alert("Please enter valid details");
+
+}
